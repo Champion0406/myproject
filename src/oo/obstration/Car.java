adf6d71bca327ce5e4e7a62d6efe6de878934e29
user <user@es302n32>
2017-03-14 15:57:06 +0800

    新增建構子

--- src/oo/obstration/Car.java
+++ src/oo/obstration/Car.java
@@ -5,11 +5,13 @@ public class Car {
 	    int cc;
 	    String name;
 	    String type;
+	    int status;
 	    public Car( String brand,int cc,String name,String type){
 	    	this.brand = brand;
 	    	this.cc = cc;
 	    	this.name = name;
 	    	this.type = type;
+	    	
 	    	
 	    	
 	    }
--- src/oo/obstration/CarTester.java
+++ src/oo/obstration/CarTester.java
@@ -4,6 +4,10 @@ public class CarTester {
 
 	public static void main(String[] args) {
 		Car c1 = new Car("Toyota",1800,"Altis","Sedan");
+		c1.cc = 1800;
+		c1.brand = "TOYOTA";
+		c1.name = "Altis";
+		
 
 	}
 
