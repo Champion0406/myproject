0cf89b35367cbea60194454ff7f31bed6b1bbcf8
user <user@es302n32>
2017-03-21 16:23:26 +0800

    Fields,car id

--- src/oo/obstration/CarTester.java
+++ src/oo/obstration/CarTester.java
@@ -6,7 +6,26 @@ public class CarTester {
 		Car c1 = new Car("Toyota",1800,"Altis","Sedan");
 		Car c2 = new Car("Honda",2000,"Accord","Sedan");
 		Car c3 = new Car("Toyota",2000,"wish","Sedan");
-
+		
+		int[] n = new int[5];
+		System.out.print(n[2]);
+		
+		Car[]cars = new Car[3];
+		cars[0] = new Car("Toyota",1800,"Altis","Sedan");
+		cars[1] = new Car("Honda",2000,"Accord","Sedan");
+		cars[2] = new Car("Toyota",2000,"wish","Sedan");
+		cars[0].id = 301;
+		cars[1].id = 302;
+		cars[2].id = 303;
+		System.out.print(cars[0].name);
+		int i=0;
+		for (i = 0;i<3;i++){
+			System.out.print(cars[i].name);
+		}
+		System.out.println(i);
+		
 	}
-
+	class AA{	
+	}
+	AA a = new AA();
 }
