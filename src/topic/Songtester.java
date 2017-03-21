fbb38ae532e41a58a445d8acf1cf61dbbf20d7ba
Champion0406 <barney0406@gmail.com>
2017-03-21 14:07:44 +0800

    Update Songtester.java

--- src/topic/Songtester.java
+++ src/topic/Songtester.java
@@ -3,5 +3,7 @@ package topic;
 public class Songtester {
 	public static void main(String[] args) {
 		Song s1 = new Song("晴天", "Jay Chou", "Jay Chou", 5, "Lyrical");
+		song s2 = new Song("愛的飛行日記","Jay Chou","方文山",4,"Lyrical");
+		song s3 = new Song("演員","薛之謙","薛之謙",5,"Lyrical");
 	}
 }
