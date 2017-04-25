5d0e251fafafe9215273c891478f9016d9e69717
user <user@es302n31>
2017-04-25 14:28:29 +0800

    mid

---
+++ src/oo/mid/Meal.java
@@ -0,0 +1,24 @@
+package oo.mid;
+
+public class Meal {
+	
+	String dessert;
+	String mainmeal;
+	String vicemeal;
+	String drink;
+	int price;
+	
+	public Meal(String dessert,String mainmeal,String vicemeal,String drink,int price ){
+		this.dessert = dessert;
+		this.drink = drink;
+		this.mainmeal = mainmeal;
+		this.vicemeal = vicemeal;
+		this.price = price;
+	public void 
+		
+	}
+	
+	
+	
+	
+}
---
+++ src/oo/mid/MealTester.java
@@ -0,0 +1,11 @@
+package oo.mid;
+
+import oo.obstration.Car;
+
+public class MealTester {
+	public static void main(String[] args) {
+		Meal m1 = new Meal("ice cream","coke","hamburger","fries",139);
+		Meal m2 = new Meal("no","black tea","hamburger","fried chicken",129);
+		Meal m3 = new Meal("apple pie","sprite","hamburger","fries",119);
+	}
+}
