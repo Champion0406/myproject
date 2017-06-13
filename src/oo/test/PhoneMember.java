ebbe68801bf24ce86c99558481523db1b1c652d3
user <user@es302n32>
2017-06-13 16:22:38 +0800

    6/13

---
+++ src/oo/test/PhoneMember.java
@@ -0,0 +1,30 @@
+package oo.test;
+
+public class PhoneMember extends Member {
+	String phone;
+	boolean varified = false;
+	public PhoneMember(String name){
+		super(name);
+	
+	}
+	public PhoneMember(){
+		super();
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public boolean isVarified() {
+		return varified;
+	}
+
+	public void setVarified(boolean varified) {
+		this.varified = varified;
+	}
+
+}
